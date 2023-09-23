54d1d7608cc4e2461559632b40a8efde4f6ed299
IThundxr <contact@ithundxr.dev>
2023-09-23 10:46:13 -0400

    teeeeeeeeeeest

--- src/main/kotlin/dev/ithundxr/silk/SilkProject.kt
+++ src/main/kotlin/dev/ithundxr/silk/SilkProject.kt
@@ -6,13 +6,11 @@ import org.eclipse.jgit.errors.RepositoryNotFoundException
 import org.gradle.api.Project
 
 class SilkProject(private val project: Project): Project by project {
-    val git: JGitWrapper? by lazy {
-        try {
-            JGitWrapper(Git.open(rootDir))
-        } catch (e: RepositoryNotFoundException) {
-            null
-        }
-    }
+   val git: JGitWrapper? by lazy {
+        try { JGitWrapper(Git.open(rootDir)) } catch (e: RepositoryNotFoundException) { null }
+   }
+
+   val changelog = ChangelogText(this)
 
    val extension: SilkGradleExtension
         get() = project.extensions.getByType(SilkGradleExtension::class.java)
